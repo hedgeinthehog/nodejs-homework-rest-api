a617502e4955d65e19957ec74edf64fce6c89b48
hedgeinthehog <37834796+hedgeinthehog@users.noreply.github.com>
2021-07-17 19:04:43 +0300

    creates model

--- model/index.js
+++ model/index.js
@@ -1,46 +1,6 @@
-const contacts = require('./contacts.json');
+const { model } = require('mongoose');
+const { contactSchema } = require('./schemas');
 
-const listContacts = async () => {
-  return contacts;
-};
+const Contact = model('contact', contactSchema);
 
-const getContactById = async (contactId) => {
-  const contact = contacts.find(
-    (contact) => contact.id.toString() === contactId
-  );
-  return contact;
-};
-
-const removeContact = async (contactId) => {
-  const contactIdx = contacts
-    .map((contact) => contact.id.toString())
-    .indexOf(contactId);
-
-  if (contactIdx === -1) return null;
-
-  contacts.splice(contactIdx, 1);
-  return 1;
-};
-
-const addContact = async (body) => {
-  contacts.push(body);
-};
-
-const updateContact = async (contactId, body) => {
-  const contact = contacts.find(
-    (contact) => contact.id.toString() === contactId
-  );
-
-  if (!contact) return null;
-
-  const updatedContact = Object.assign(contact, body);
-  return updatedContact;
-};
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+module.exports = { Contact };
