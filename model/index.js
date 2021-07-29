8875f3de20bb012e676765128cb7185f9f17d18d
unknown <you@example.com>
2021-07-29 15:26:16 +0300

    creates a user model

--- model/index.js
+++ model/index.js
@@ -1,6 +1,8 @@
 const { model } = require('mongoose');
 const { contactSchema } = require('./schemas');
+const { userSchema } = require('./schemas');
 
 const Contact = model('contact', contactSchema);
+const User = model('user', userSchema);
 
-module.exports = { Contact };
+module.exports = { Contact, User };
--- model/schemas/contact.js
+++ model/schemas/contact.js
@@ -1,4 +1,4 @@
-const { Schema } = require('mongoose');
+const { Schema, SchemaTypes } = require('mongoose');
 
 const contactSchema = new Schema(
   {
@@ -25,6 +25,10 @@ const contactSchema = new Schema(
       type: Boolean,
       default: false,
     },
+    owner: {
+      type: SchemaTypes.ObjectId,
+      ref: 'user',
+    }
   },
   { versionKey: false, timestamps: true }
 );
