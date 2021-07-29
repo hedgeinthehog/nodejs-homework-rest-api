c735d45693ff42fe4153221f7efaff121729be29
unknown <you@example.com>
2021-07-29 15:33:50 +0300

    updates contacts services

--- services/contacts.js
+++ services/contacts.js
@@ -1,18 +1,34 @@
 const { Contact } = require('../model');
 
-const listContacts = async () => Contact.find({});
+const listContacts = async () => {
+  const result = await Contact.find({});
+  return result;
+};
 
-const getContactById = async (id) => Contact.findById({ _id: id });
+const getContactById = async (id) => {
+  const result = await Contact.findById({ _id: id });
+  return result;
+};
 
-const createContact = async (contact) => Contact.create(contact);
+const createContact = async (contact, userId) => {
+  const result = await Contact.create({...contact, owner: userId});
+  return result;
+};
 
-const updateContact = async (id, fields) =>
-  Contact.findByIdAndUpdate({ _id: id }, fields, { new: true });
+const updateContact = async (id, fields) => {
+  const result = await Contact.findByIdAndUpdate({ _id: id }, fields, { new: true });
+  return result;
+};
 
-const removeContact = async (id) => Contact.findByIdAndRemove({ _id: id });
+const removeContact = async (id) => {
+  const result = await Contact.findByIdAndRemove({ _id: id });
+  return result;
+};
 
-const updateFavorite = async (id, { favorite }) =>
-  Contact.findByIdAndUpdate({ _id: id }, { favorite }, { new: true });
+const updateFavorite = async (id, { favorite }) => {
+  const result = Contact.findByIdAndUpdate({ _id: id }, { favorite }, { new: true });
+  return result;
+};
 
 module.exports = {
   listContacts,
